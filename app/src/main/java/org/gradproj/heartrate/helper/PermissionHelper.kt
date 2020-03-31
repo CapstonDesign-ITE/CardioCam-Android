4d64fc567a96ab9b31d1e8b1db3afc242b22f87d
GowoonJ <jgw971229@naver.com>
2020-03-31 23:35:58 +0900

    add permission library

---
+++ app/src/main/java/org/gradproj/heartrate/helper/PermissionHelper.kt
@@ -0,0 +1,36 @@
+package org.gradproj.heartrate.helper
+
+import android.Manifest
+import android.app.Activity
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+
+
+class PermissionHelper(val activity: Activity) : TedPermission() {
+    var permissionCheckable : Boolean = false
+
+    fun constructor(){
+        val permissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                Toast.makeText(
+                    activity, "Permission Granted", Toast.LENGTH_SHORT
+                ).show()
+                permissionCheckable = true
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String?>) {
+                Toast.makeText(
+                    activity, "Permission Denied", Toast.LENGTH_SHORT
+                ).show()
+                permissionCheckable = false
+            }
+        }
+
+        with(activity)
+            .setPermissionListener(permissionListener)
+            .setDeniedMessage("권한을 허용해야 모든 기능을 사용할 수 있습니다.")
+            .setPermissions(Manifest.permission.CAMERA)
+            .check()
+    }
+}

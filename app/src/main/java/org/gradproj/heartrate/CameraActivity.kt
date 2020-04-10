231f965417f686a2ae151890878b0705bdc6c920
GowoonJ <jgw971229@naver.com>
2020-04-10 13:59:24 +0900

    cameraBind edit

--- app/src/main/java/org/gradproj/heartrate/CameraActivity.kt
+++ app/src/main/java/org/gradproj/heartrate/CameraActivity.kt
@@ -1,26 +1,29 @@
 package org.gradproj.heartrate
 
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.TextureView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraXConfig
 import androidx.camera.core.Preview
 import androidx.lifecycle.LifecycleOwner
 import org.gradproj.heartrate.helper.PermissionHelper
+import java.lang.Math.*
 import java.util.concurrent.Executors
+import kotlin.math.absoluteValue
 
 class CameraActivity : AppCompatActivity(), LifecycleOwner, CameraXConfig.Provider{
 
-    val permissionHelper : PermissionHelper = PermissionHelper(this);
+    val TAG : String = "CAMERA_ACTIVITY"
+    val permissionHelper : PermissionHelper = PermissionHelper(this)
 
-    val cameraManager : CameraManager? = null
-    val cameraDevice : CameraDevice? = null
     var previewed = false
 
+    // Initialize background executor
     private val executor = Executors.newSingleThreadExecutor()
     private lateinit var viewFinder : TextureView
 
@@ -29,6 +32,10 @@ class CameraActivity : AppCompatActivity(), LifecycleOwner, CameraXConfig.Provid
         setContentView(R.layout.activity_main)
 
         viewFinder = findViewById(R.id.textureView)
+        viewFinder.post{
+            //Build UI control
+            // Bind use cases
+        }
 
         if (permissionHelper.permissionCheckable){
             viewFinder.post{startCamera()}
@@ -40,6 +47,8 @@ class CameraActivity : AppCompatActivity(), LifecycleOwner, CameraXConfig.Provid
         viewFinder.addOnLayoutChangeListener { view, i, i2, i3, i4, i5, i6, i7, i8 ->
             updateTransform()
         }
+
+
     }
 
     private fun flashControl(){
@@ -50,11 +59,27 @@ class CameraActivity : AppCompatActivity(), LifecycleOwner, CameraXConfig.Provid
 //        else imageCapture.flashMode = ImageCapture.FLASH_MODE_ON
     }
 
+    private fun bindCameraUseCases(){
+        val metrics = DisplayMetrics().also {
+            viewFinder.display.getRealMetrics(it)
+        }
+        Log.d(TAG, "${metrics.widthPixels} x ${metrics.heightPixels}" )
+
+        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - AspectRatio.RATIO_4_3.absoluteValue) <= abs(previewRatio - AspectRatio.RATIO_16_9)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
     // initializer cameraX
     override fun getCameraXConfig(): CameraXConfig {
         return Camera2Config.defaultConfig()
     }
-
 
     // camera open
     private fun startCamera(){

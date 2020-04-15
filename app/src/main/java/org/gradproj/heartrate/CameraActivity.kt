c216468c3ef6e1db3b043aa3cb1047e291ac9ea3
GowoonJ <jgw971229@naver.com>
2020-04-15 12:40:36 +0900

    cameraX 구동 확인

--- app/src/main/java/org/gradproj/heartrate/CameraActivity.kt
+++ app/src/main/java/org/gradproj/heartrate/CameraActivity.kt
@@ -1,5 +1,9 @@
 package org.gradproj.heartrate
 
+import android.Manifest
+import android.content.Context
+import android.content.res.Configuration
+import android.hardware.display.DisplayManager
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
@@ -9,8 +13,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
-import org.gradproj.heartrate.helper.PermissionHelper
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import java.lang.Math.*
 import java.nio.ByteBuffer
 import java.util.*
@@ -19,12 +23,12 @@ import java.util.concurrent.Executors
 import kotlin.collections.ArrayList
 import kotlin.math.absoluteValue
 
+
 typealias LumaListener = (luma:Double) -> Unit
 
-class CameraActivity : AppCompatActivity(), LifecycleOwner{
+class CameraActivity : AppCompatActivity(){
 
     val TAG : String = "CAMERA_ACTIVITY"
-    val permissionHelper : PermissionHelper = PermissionHelper(this)
 
     var previewed = false
 
@@ -41,6 +45,10 @@ class CameraActivity : AppCompatActivity(), LifecycleOwner{
 
     private lateinit var cameraExecutor : ExecutorService
 
+    private val displayManager by lazy {
+        this.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -50,13 +58,39 @@ class CameraActivity : AppCompatActivity(), LifecycleOwner{
         viewFinder.post{
             //Build UI control
             // Bind use cases
-        }
+            }
 
-        if (permissionHelper.permissionCheckable){
-            Toast.makeText(this, "권한 허용 확인", Toast.LENGTH_SHORT).show()
-        } else{
-            Toast.makeText(this, "권한 허가 후 다시 시도해주세요", Toast.LENGTH_SHORT).show()
-            finish()
+        val permissionListener: PermissionListener =
+            object : PermissionListener {
+                override fun onPermissionGranted() {
+                    Toast.makeText(this@CameraActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+                }
+                override fun onPermissionDenied(deniedPermissions: List<String?>) {
+                    Toast.makeText(
+                        this@CameraActivity, "Permission Denied\n$deniedPermissions", Toast.LENGTH_SHORT).show()
+                }
+            }
+        TedPermission.with(this)
+            .setPermissionListener(permissionListener)
+            .setDeniedMessage("권한 허용 거부시 기능을 정상적으로 이용할 수 없습니다.")
+            .setPermissions(Manifest.permission.CAMERA)
+            .check()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+//        updateCameraUI()
+    }
+
+    private val displayListener = object : DisplayManager.DisplayListener {
+        override fun onDisplayAdded(displayId: Int) = Unit
+        override fun onDisplayRemoved(displayId: Int) = Unit
+        override fun onDisplayChanged(displayId: Int) = viewFinder.let { view ->
+            if (displayId == this@CameraActivity.displayId) {
+                Log.d(TAG, "Rotation changed: ${view.display.rotation}")
+                imageCapture?.targetRotation = view.display.rotation
+                imageAnalyzer?.targetRotation = view.display.rotation
+            }
         }
     }
 

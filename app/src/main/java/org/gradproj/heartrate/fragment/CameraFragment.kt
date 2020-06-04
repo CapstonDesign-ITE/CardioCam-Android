8ef85f687f267e3d8901843c00c4da2bff5eba00
GowoonJ <jgw971229@naver.com>
2020-06-04 17:38:32 +0900

    add yuv to Rgb converter

--- app/src/main/java/org/gradproj/heartrate/fragment/CameraFragment.kt
+++ app/src/main/java/org/gradproj/heartrate/fragment/CameraFragment.kt
@@ -7,6 +7,7 @@ import android.hardware.display.DisplayManager
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup

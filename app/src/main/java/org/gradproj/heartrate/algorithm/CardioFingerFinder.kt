9d8a31d8732c8d5f194cd875c0d9e29543da09b1
ykyu99 <ykyu99@naver.com>
2020-07-22 11:29:30 +0900

    7.1 주석달아놓은버전

--- app/src/main/java/org/gradproj/heartrate/algorithm/CardioFingerFinder.kt
+++ app/src/main/java/org/gradproj/heartrate/algorithm/CardioFingerFinder.kt
@@ -3,20 +3,20 @@ package org.gradproj.heartrate.algorithm
 import android.graphics.Camera
 
 //메인
-val size : Int = 30
-var t : Int = 0
-var tMin: Int = 0
-var tMax: Int = 0
-var r : Array<Double?> = Array(size) { null}
-var g : Array<Double?> = Array(size) { null}
-var b : Array<Double?> = Array(size) { null}
-var diffR : Array<Double?> =  Array(size) { null}
-var maxR : Double = 0.0
-var minR : Double = 0.0
-var hI : Array<Double?> = Array (size){null}
-var s : Double= 0.0
+val size : Int = 30 // 프레임 사이즈
+var t : Int = 0 // 프레임 번호
+var tMin: Int = 0 // 최소값을 갖는 프레임
+var tMax: Int = 0 // 최대값을 갖는 프레임
+var r : Array<Double?> = Array(size) { null} // 빨강영역 프레임 별 강도
+var g : Array<Double?> = Array(size) { null} // 초록영역 프레임 별 강도
+var b : Array<Double?> = Array(size) { null} // 파랑영역 프레임 별 강도
+var diffR : Array<Double?> =  Array(size) { null} // diffR 값
+var maxR : Double = 0.0 // diffR 중 최대
+var minR : Double = 0.0 // diffR 중 최소
+var hI : Array<Double?> = Array (size){null} //diffR의 분포
+var s : Double= 0.0 // pr 값이 저장된 곳
 var k : Int = 0
-var standardValley : Int = 0
+var standardValley : Int = 0 // 수축- 확장 최대를 구하기 위한 기준 벨리
 
 
 val x : Int = 1280
@@ -37,11 +37,11 @@ if (startOrNot) {
 
         sPre= s
         var FL :Double = camera.flashlight
-        assessmentScore()
-        var Feedback = s - sPre
+            assessmentScore()
+            var Feedback = s - sPre
 
-        if (FL-FLPre > 0.85)
-        {
+            if (FL-FLPre > 0.85)
+            {
             FLPre = FL
             var OffsetFL:Double = Feedback * 0.05
             FL = FLPre + OffsetFL
